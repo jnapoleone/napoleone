3f8921239e880ed42480b6e894520f6e20ccfe5e
John Napoleone <john.napoleon@gmail.com>
2015-01-15 21:00:19 -0500

    Vagrant import

--- sites/all/themes/napoleone/js/script.js
+++ sites/all/themes/napoleone/js/script.js
@@ -21,5 +21,99 @@ Drupal.behaviors.my_custom_behavior = {
   }
 };
 
+Drupal.behaviors.moreButton = {
+  attach: function(context, settings) {
 
+    var $nextLink = $('.p-next');
+
+    $nextLink.on( 'click', function () {
+      var $nextProject = $(this).closest('.p__section').next();
+      $('html, body').scrollTo($nextProject,500);
+      //$("html, body").animate({
+      //  scrollTop: $closestProject.next().offset().bottom
+      //},1000);
+    });
+  }
+};
+
+Drupal.behaviors.projectDTInView = {
+  attach: function(context,settings) {
+
+    $('.p-dt-description').bind('inview', function(e, isInView, visiblePartX, visiblePartY){
+      if(isInView) {
+
+        //if(visiblePartY=='bottom'){
+
+          $('.l-header').addClass('p-dt__header');
+        //}
+      } else {
+        $('.l-header').removeClass('p-dt__header');
+      }
+    });
+  }
+};
+
+Drupal.behaviors.projectRTTInView = {
+  attach: function(context,settings){
+    $('.p-rtt-description').bind('inview', function(e, isInView, visiblePartX, visiblePartY){
+      if(isInView) {
+
+        //if(visiblePartY=='bottom'){
+
+          $('.l-header').addClass('p-rtt__header');
+        //}
+      }else {
+        $('.l-header').removeClass('p-rtt__header');
+      }
+    });
+  }
+};
+
+Drupal.behaviors.projectTTInView = {
+  attach: function(context,settings){
+    $('.p-tt-description').bind('inview', function(e, isInView, visiblePartX, visiblePartY){
+      if(isInView) {
+
+        //if(visiblePartY=='bottom'){
+
+          $('.l-header').addClass('p-tt__header');
+        //}
+      }else {
+        $('.l-header').removeClass('p-tt__header');
+      }
+    });
+  }
+};
+
+Drupal.behaviors.projectTWIGInView = {
+  attach: function(context,settings){
+    $('.p-twig-description').bind('inview', function(e, isInView, visiblePartX, visiblePartY){
+      if(isInView) {
+
+        //if(visiblePartY=='bottom'){
+
+          $('.l-header').addClass('p-twig__header');
+        //}
+      }else {
+        $('.l-header').removeClass('p-twig__header');
+      }
+    });
+  }
+};
+
+Drupal.behaviors.projectRTEInView = {
+  attach: function(context,settings){
+    $('.p-rte-description').bind('inview', function(e, isInView, visiblePartX, visiblePartY){
+      if(isInView) {
+
+        //if(visiblePartY=='bottom'){
+
+          $('.l-header').addClass('p-rte__header');
+        //}
+      }else {
+        $('.l-header').removeClass('p-rte__header');
+      }
+    });
+  }
+};
 })(jQuery, Drupal, this, this.document);
--- sites/all/themes/napoleone/layouts/panels/simple-site-template/simple-site-template.tpl.php
+++ sites/all/themes/napoleone/layouts/panels/simple-site-template/simple-site-template.tpl.php
@@ -1,3 +1,6 @@
+<header class="l-region--header">
+  <div class="l-header"><?php print $content['header']; ?></div>
+</header>
 <div class="l-main">
   <?php print $content['main']; ?>
 </div>
